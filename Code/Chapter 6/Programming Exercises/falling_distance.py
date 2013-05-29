7250b5d91936a48df31474e8f720b2010f285aad
techabyte <techabyte@techabyte.kd.io>
2013-05-29 16:19:51 +0000

    Updated with M and format

--- Code/Chapter 6/Programming Exercises/falling_distance.py
+++ Code/Chapter 6/Programming Exercises/falling_distance.py
@@ -3,9 +3,9 @@
 import math
 
 def main():
-	for count in range(1, 10):
+	for count in range(1, 11):
 		dis = falling_distance(count)
-		print(dis)
+		print('The falling distance is ', format(dis, ',.2f'), 'M', sep='')
 
 def falling_distance(count):
 	d = (0.5) * (9.8) * (count**2)
