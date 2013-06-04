57bc3f68461c58995f387b7516c579df0df39c97
techabyte <techabyte@gmail.com>
2013-06-04 18:43:13 +1000

    Added

---
+++ Code/Chapter 6/Programming Exercises/prime_numbers.py
@@ -0,0 +1,20 @@
+# Programming Excerises
+# Prime Numbers
+import math
+import random
+
+def main():
+	num = int(input('Enter Number: '))
+	result = is_prime(num)
+	if result == True:
+		print(num, 'is a prime number')
+	else:
+		print(num, 'not a prime number')
+
+def is_prime(num):
+	for x in range(2, int(num**0.5)+1):
+		if num % x == 0:
+			return False
+	return True
+
+main()
