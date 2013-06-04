8d41e33382ab82643714b263e39050929ab60bd0
techabyte <techabyte@gmail.com>
2013-06-04 17:01:07 +1000

    Updated

--- Code/Chapter 6/Programming Exercises/odd_even_counter.py
+++ Code/Chapter 6/Programming Exercises/odd_even_counter.py
@@ -3,4 +3,17 @@
 import random
 
 def main():
-	
+	print('Now generating 100 random numbers \nCalculating ' \
+			'how many of them are odd and even..........')
+	for count in range(1):
+		even = 0
+		odd = 0
+		for count in range(100):
+			number = random.randint(1, 100)
+			if number % 2 == 0:
+				even += 1
+			else:
+				odd += 1
+	print('Even:', even, 'Odd:', odd)
+
+main()
