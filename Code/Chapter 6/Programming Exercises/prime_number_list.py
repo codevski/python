4c2473805419e32891c10b3b9a84733590af5406
techabyte <techabyte@gmail.com>
2013-06-05 19:32:04 +1000

    Updated

--- Code/Chapter 6/Programming Exercises/prime_number_list.py
+++ Code/Chapter 6/Programming Exercises/prime_number_list.py
@@ -29,7 +29,8 @@ def main():
 			print(number)
 		
 def is_prime(num):
-	if num % 2 == 0:
-		return False
+	for x in range(2, int(num**0.5)+1):
+		if num % x == 0:
+			return False
 	return True
 main()
