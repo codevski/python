c95dd585f5f38f82eee2a0c99f58cf4b4c8eaa2a
techabyte <techabyte@gmail.com>
2013-06-06 17:06:07 +1000

    Added

---
+++ Code/Chapter 6/Programming Exercises/rock_paper_scissors.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  rock_paper_scissors.py
+#  
+#  Copyright 2013 techabyte <admin@techabyte.com.au>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import random
+ROCK = 1
+PAPER = 2
+SCISSORS = 3
+
+def main():
+	x = random.randint(1, 3)
+	guess = int(input('rock, paper or scissors?: '))
+
+if __name__ == '__main__':
+	main()
+
